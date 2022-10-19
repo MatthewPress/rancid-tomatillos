4fcc56994e88a69b00d7c65f9f60a34c553ae225
Michael Martinelli <turtelboy123@gmail.com>
2022-10-19 14:39:15 -0600

    Feat/CYOA:Search Bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import Header from '../Header/Header';
 import MovieCard from '../MovieCard/MovieCard';
 import Moviebox from "../Moviebox/Moviebox";
+import NavBar from "../Navbar/NavBar";
+import Error from "../Error/Error";
 
 import { getMovies, getSingleMovie } from '../../apiCalls/apiCalls';
 
@@ -12,13 +14,15 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState({});
-
+  const [currentResults, setSearchResults] = useState([])
+  const [error, setError] = useState('')
+  const [searchInput, setInput] = useState("")
   useEffect(() => {
     getMovies()
       .then((movieData) => {
         setMovies(movieData.movies);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => setError(error));
   }, []);
 
   const handleSelection = (event) => {
@@ -31,9 +35,17 @@ function App() {
       })
       .catch((error) => console.log(error));
   }
+  const handleInput = (event) => {
+    const results = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    setInput(event.target.value)
+    setSearchResults(results)
+  };
 
   const clearSelection = () => {
     setSelectedMovie({});
+    setInput('');
   }
 
   return (
@@ -42,21 +54,46 @@ function App() {
         exact
         path="/"
         render={() =>
-          <div className="App">
-            <Header />
-            <Moviebox movies={movies} handleSelection={handleSelection} className="MovieBox" />
-          </div>
-
+          error ? (
+            <Error />
+          ) : searchInput ? (
+            < div className="App">
+              <Header />
+              <NavBar
+                className="Search-Bar"
+                handleInput={handleInput}
+                movies={movies}
+              />
+              {currentResults.length ? (
+                <div>
+                  <h2>seach results for '{searchInput}'</h2>
+                  <Moviebox movies={currentResults} handleSelection={handleSelection} />
+                </div>
+              ) : (
+                <h2>no matching results. . .</h2>
+              )}
+            </div>
+          ) : (
+            <div className="App">
+              <Header />
+              <NavBar
+                className="Search-Bar"
+                handleInput={handleInput}
+                movies={movies}
+              />
+              <Moviebox movies={movies} handleSelection={handleSelection} className="MovieBox" />
+            </div>
+          )
         }
       />
-      <Route
+      < Route
         exact
         path="/movie/:id"
         render={() => <div className="Movie-Description">
           <Header />
           <MovieCard selectedMovie={selectedMovie} clearSelection={clearSelection} />
         </div>} />
-    </Switch>
+    </Switch >
   );
 }
 
